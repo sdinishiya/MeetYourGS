a0a2decb34fcfee7f0adfd17fbc85fb3ca00b923
Maheshi Yatipansalawa <messi98ky@gmail.com>
2021-07-13 12:55:46 +0530

    present projects

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import login from './components/pages/login';
 import SignUp from './components/pages/SignUp';
 import ForumDiscussion from './components/Forum/admin_viewforum';
 import ForumView from './components/Forum/public_viewforum';
+import ProjectReviewCard from './components/projects/cards/PresentProjects';
+import CenteredGrid from './components/projects/grids/PresentProjects'; 
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
           <Route path='/sign-up' component={SignUp} />
           <Route path='/admin_viewforum' component={ForumDiscussion} />
           <Route path='/public_viewforum' component={ForumView} />
+          <Route exact path="/projects/cards/PresentProjects" component = {ProjectReviewCard} />
+          <Route exact path="/projects/grids/PresentProjects" component = {CenteredGrid} />
         </Switch>
       </Router>
     </>
