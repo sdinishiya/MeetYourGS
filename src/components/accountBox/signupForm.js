449e525f11a7f765c3a05d874bdaa2997cd6b419
hiradev <hirushachamod@gmail.com>
2021-07-14 06:39:25 +0530

    email send

--- src/components/accountBox/signupForm.js
+++ src/components/accountBox/signupForm.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import Axios from 'axios';
+import emailjs from "emailjs-com";
 import "../../App.css";
 
 import {
@@ -12,6 +13,18 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
+
+function sendEmail(e) {
+  e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_ath4afo', e.target, 'user_LyuIHw84iO7FKuqlwwC5S')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+}
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
@@ -38,17 +51,17 @@ export function SignupForm(props) {
  
   return (
     <BoxContainer>
-      <FormContainer>
-      <Input type="text" placeholder="Full Name" onChange={(e) => { setFullnameReg(e.target.value) }} />
+      <FormContainer onSubmit={sendEmail}>
+      <Input type="text" placeholder="Full Name" name="to_name" onChange={(e) => { setFullnameReg(e.target.value) }} />
         <Input type="text" placeholder="Permanent Address" onChange={(e) => { setAddressReg(e.target.value) }} />
         <Input type="text" placeholder="NIC" onChange={(e) => { setNicReg(e.target.value) }} />
         <Input type="text" placeholder="Telephone No" onChange={(e) => { setTelReg(e.target.value) }} />
-        <Input type="email" placeholder="Email" onChange={(e) => { setEmailReg(e.target.value) }} />
+        <Input type="email" placeholder="Email" name="to_email" onChange={(e) => { setEmailReg(e.target.value) }} />
         <Input type="password" placeholder="Password" onChange={(e) => { setPasswordReg(e.target.value) }} />
         <Input type="password" placeholder="Confirm Password" /*onChange={(e) => { setConpassReg(e.target.value) }}*/ />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit" onClick={signup}>Signup</SubmitButton>
+      <SubmitButton type="submit" onClick={signup} >Signup</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
