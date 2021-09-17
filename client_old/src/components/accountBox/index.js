d321ba81296ce3c8265011c88ddc70a6c785d407
MohomedSalman <mohamedsalmandeen@gmail.com>
2021-09-17 15:02:46 +0530

    Updated

--- client_old/src/components/accountBox/index.js
+++ client_old/src/components/accountBox/index.js
@@ -5,17 +5,7 @@ import { motion } from "framer-motion";
 import { AccountContext } from "./accountContext";
 import { SignupForm } from "./signupForm";
 
-const BoxContainer = styled.div`
-  width: 280px;
-  min-height: 550px;
-  display: flex;
-  flex-direction: column;
-  border-radius: 19px;
-  background-color: #fff;
-  box-shadow: 0 0 2px rgba(02, 02, 02, 0.28);
-  position: relative;
-  overflow: hidden;
-`;
+
 
 const TopContainer = styled.div`
   width: 100%;
